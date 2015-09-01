304f9f63d354dd964179851924a77567c099b57b
William Claude Tumeo <tumeowilliam@gmail.com>
2015-08-31 21:48:24 -0300

    Fix: remove deprecated method

--- background.js
+++ background.js
@@ -2,14 +2,13 @@
 var twitch = {
     name: 'Cogumelando',
     username: 'cogumelandooficial',
-    ajaxInterval: 300000 // 5 minutos
+    ajaxInterval: 180000//300000 // 5 minutos
 }
 
 noConnect();
 getTwitch(twitch.username);
 twitch.mainLoop = setInterval(function(){
     getTwitch(twitch.username);
-    setTimeout(mythTwitch,5000);
 },twitch.ajaxInterval);
 
 function mythTwitch(twitchJson){
@@ -21,10 +20,12 @@ function mythTwitch(twitchJson){
         chrome.browserAction.setBadgeText({text:"LIVE"});
         chrome.browserAction.setBadgeBackgroundColor({color: "#0d0"});
         chrome.browserAction.setTitle({title:twitch.name+' | '+twitch.game});
+        console.log("Stream");
     }else{
         chrome.browserAction.setBadgeText({text:"OFF"});
         chrome.browserAction.setBadgeBackgroundColor({color: "#d00"});
         chrome.browserAction.setTitle({title:twitch.name+' | Off-air'});
+        console.log("Off-air");
     }
 }
 
@@ -32,9 +33,11 @@ function getTwitch(username){
     $.ajax({
         url:'https://api.twitch.tv/kraken/streams/'+username,
         success:function(channel) {
+            console.log('Objeto recebido');
             mythTwitch(channel);
         },
         error:function() {
+            console.log('Sem conexão');
             noConnect();
         }
     });
