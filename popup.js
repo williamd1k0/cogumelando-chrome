e31aae8efaaccd13482764eed39a4e4a441cc442
William Claude Tumeo <tumeowilliam@gmail.com>
2015-09-04 01:11:24 -0300

    Ajustes no popup - Texto dos botões onhover - Highlight no botão do twitch - Add disable sound label

--- popup.js
+++ popup.js
@@ -1,27 +1,38 @@
-/* @deprecated
-var mythTwitch = chrome.extension.getBackgroundPage();
-console.log("Retorno do background "+mythTwitch);
 
-try{
-    var stream = mythTwitch.twitch.getChannel();
-    console.log("Stream "+stream);
-    if(stream){
-        console.log("integração popup/background");
-    }
-}catch(e){}
-*/
+var buttons = document.getElementsByClassName('corolho'),
+    menu = document.getElementById('menu'),
+    buttonsHelp = ['Canal do Twitch','Página do Facebook','Site oficial','Opções'],
+    disableSound = document.getElementById('sound');
+buttons[0].className = 'corolho';
 
 if(localStorage.onStream){
-    var stream = JSON.parse(localStorage.channel);
-    var twitchView = document.getElementsByClassName('twitch');
+    var stream = JSON.parse(localStorage.channel),
+        twitchView = document.getElementsByClassName('twitch');
+
+    buttons[0].className = 'corolho live';
+    buttons[0].focus();
     twitchView[0].innerHTML = '<p>'+stream.game+'</p>';
     twitchView[1].innerHTML = `
         <p>
             <img style="width:95%" src="http://static-cdn.jtvnw.net/previews-ttv/live_user_cogumelandooficial-320x180.jpg">
         </p>`;
-    twitchView[2].innerHTML = '<p><small>'+stream.channel.status+'</small></p>';
+    twitchView[2].innerHTML = '<p>'+stream.channel.status+'</p>';
 }
-var disableSound = document.getElementById('sound');
+
+for (var i = 0; i < buttons.length; i++) {
+    setCursorEvent(buttons[i],buttonsHelp[i]);
+}
+
+function setCursorEvent(element, help){
+    element.onmouseover = function(){
+        menu.innerHTML = '<small>'+help+'</small>';
+    }
+    element.onmouseout = function(){
+        menu.innerHTML = '';
+    }
+}
+
+
 disableSound.checked = localStorage.sound ? false : true;
 disableSound.onchange = function(){
     if(this.checked){
