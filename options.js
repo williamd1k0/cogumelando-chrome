578522a7d8103da4f825606c4e07ca0772de1608
William Claude Tumeo <tumeowilliam@gmail.com>
2015-09-05 02:29:38 -0300

    Update configs page
    - Add loop buttons
    - Add reset button

--- options.js
+++ options.js
@@ -6,20 +6,58 @@ for (var i = 0; i < options.length; i++) {
     }
 }
 
-var sounds = document.getElementsByName('sound');
-var notify = document.getElementsByName('notify');
+var sounds = document.getElementsByName('sound'),
+    notify = document.getElementsByName('notify'),
+    loop = document.getElementsByName('loop'),
+    reset = document.getElementById('reset'),
+    minutes = [1,2,5,10,20,30];
 
 checkStorage(sounds,'sound');
 checkStorage(notify,'notify');
-
+checkLoop();
 configButton(sounds,'sound');
 configButton(notify,'notify');
+
+for (var i = 0; i < loop.length; i++) {
+    configLoop(loop[i],minutes[i]);
+}
+
+reset.onclick = mythReset;
 
 function checkStorage(element, storage){
     if(localStorage[storage]){
         element[0].className = "pressed";
     }else{
         element[1].className = "pressed";
+    }
+}
+
+function configLoop(element, min){
+    element.onclick = function(){
+        this.className = "pressed";
+        localStorage.setItem('interval',min);
+        for (var i = 0; i < loop.length; i++) {
+            if(loop[i].innerHTML !== min+" min"){
+                loop[i].className = '';
+            }
+        }
+    }
+}
+
+function checkLoop(){
+    var loopMin = parseInt(localStorage.interval);
+    if(loopMin == 1){
+        loop[0].className = "pressed";
+    }else if(loopMin == 2){
+        loop[1].className = "pressed";
+    }else if(loopMin == 5){
+        loop[2].className = "pressed";
+    }else if(loopMin == 10){
+        loop[3].className = "pressed";
+    }else if(loopMin == 20){
+        loop[4].className = "pressed";
+    }else if(loopMin == 30){
+        loop[5].className = "pressed";
     }
 }
 
@@ -34,4 +72,13 @@ function configButton(element,storage){
         this.className = "pressed";
         element[0].className = '';
     };
+}
+
+function mythReset(){
+    localStorage.clear();
+    localStorage.setItem('persist',true);
+    localStorage.setItem('sound',true);
+    localStorage.setItem('notify',true);
+    localStorage.setItem('interval',1);
+    window.location.reload();
 }
