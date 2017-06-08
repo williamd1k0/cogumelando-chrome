381554e24db88cee29e829a7870f6a87a206bca9
William Tumeo <tumeowilliam@gmail.com>
2017-06-08 05:04:11 -0300

    Super refactoring + fix channel username

--- scripts/init.js
+++ scripts/init.js
@@ -1,7 +1,10 @@
 
 chrome.runtime.onInstalled.addListener(function(details){
-    chrome.alarms.create("mainLoop", {delayInMinutes: 0.3, periodInMinutes: 1});
-    mythInit();
+    chrome.alarms.create("mainLoop", {
+        delayInMinutes: 1,
+        periodInMinutes: 1
+    });
+    backgroundInit();
 });
 
 
@@ -10,17 +13,20 @@ chrome.runtime.onStartup.addListener(function (){
     // força o estado da live pra Off-air por causa do popup
     localStorage.removeItem('onStream');
     // primeira inicialização
-    mythInit();
+    backgroundInit();
     // altera o botão do popup
     noConnect();
     // cria o evento de alarm (loop principal de checagem)
     // delayInMinutes: tempo antes da primeira checagem
     // periodInMinutes: tempo entre uma checagem e outra
-    chrome.alarms.create("mainLoop", {delayInMinutes: 0.3, periodInMinutes: parseInt(localStorage.interval)});
+    chrome.alarms.create("mainLoop", {
+        delayInMinutes: 1,
+        periodInMinutes: parseInt(localStorage.interval)
+    });
 });
 
 // bloco que será executado a cada X minutos
 chrome.alarms.onAlarm.addListener(function(alarm) {
     // checagem do canal do twitch
-    getTwitch(twitch.username, twitch.clientId);
+    twitchProcess(CONFIG.username, CONFIG.clientId);
 });
